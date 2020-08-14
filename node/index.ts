46fd27066964a38041598b303b4539d3e06c12d0
Beatriz Miranda <beatriza.dmiranda@gmail.com>
2020-08-14 02:50:18 -0300

    issue#7

--- node/index.ts
+++ node/index.ts
@@ -9,6 +9,7 @@ import {
 import { Clients } from './clients'
 import { analytics } from './handlers/analytics'
 import { updateLiveUsers } from './event/liveUsersUpdate'
+import { productList } from './resolvers/products';
 
 // Create a LRU memory cache for the Status client.
 // The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
@@ -46,6 +47,13 @@ export default new Service<Clients, State, ParamsContext>({
   },
   events: {
     liveUsersUpdate: updateLiveUsers,
+  },
+  graphql: {
+    resolvers: {
+      Query: {
+        productList,
+      },
+    },
   },
   routes: {
     analytics: method({
